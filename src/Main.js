018adc949427deaee210086db9a208811ee05c80
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-02-06 21:41:40 +0530

    a little cooler

--- src/Main.js
+++ src/Main.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const Preact = require("preact");
-exports.Hello = ({ name }) => (Preact.createElement("div", { class: "text-white mb-6" },
+exports.Hello = ({ name }) => (Preact.createElement("div", { class: "text-white mb-8 transition duration-300 transform hover:skew-x-6 hover:scale-110 cursor-pointer" },
     Preact.createElement("div", { class: "text-4xl" },
         "Hello, ",
         name,
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,7 +1,7 @@
 import * as Preact from 'preact';
 
 export const Hello = ({ name }: { name: string }) => (
-  <div class="text-white mb-6">
+  <div class="text-white mb-8 transition duration-300 transform hover:skew-x-6 hover:scale-110 cursor-pointer">
     <div class="text-4xl">Hello, {name}!</div>
     <div class="text-lg">- From TypeScript</div>
   </div>
