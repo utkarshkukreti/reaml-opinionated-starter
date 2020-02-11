d69726809fd67b9b242e5bde9d3606743665379b
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-02-11 13:39:15 +0530

    not sure why I'm tweaking it at this point

--- src/Main.ml
+++ src/Main.ml
@@ -15,7 +15,7 @@ module Button = struct
       [ R.class_
           "px-6 sm:px-10 py-5 bg-white font-semibold text-purple-600 sm:text-lg \
            rounded-full shadow-lg focus:outline-none transition duration-150 transform \
-           hover:-translate-y-1 hover:bg-gray-300"
+           hover:-translate-y-1 hover:bg-gray-300 hover:scale-105"
       ; R.onClick (fun _ -> setCount (count + 1))
       ]
       [ text |> Js.String.replace "{}" (string_of_int count) |> R.string ]
