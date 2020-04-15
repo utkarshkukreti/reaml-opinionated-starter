2022f863f80553f051668b11c0a74d070462ac59
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-04-15 23:53:06 +0530

    tweak colors a bit for @tailwindcss/ui

--- src/Main.js
+++ src/Main.js
@@ -1,5 +1,5 @@
 import * as Preact from 'preact';
-export const Hello = ({ name }) => (Preact.createElement("div", { class: "text-white mb-8 transition duration-300 transform hover:skew-x-6 hover:scale-110 cursor-pointer bg-purple-600 px-8 py-4 rounded shadow" },
+export const Hello = ({ name }) => (Preact.createElement("div", { class: "text-white mb-8 transition duration-300 transform hover:skew-x-6 hover:scale-110 cursor-pointer bg-purple-700 px-8 py-4 rounded shadow" },
     Preact.createElement("div", { class: "text-4xl" },
         "Hello, ",
         name,
